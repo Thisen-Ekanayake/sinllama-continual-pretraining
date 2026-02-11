ec7714a455dc978d06119cb24668fa932eee1739
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2026-02-11 18:31:27 +0530

    check bf16 model

--- projects/check_merge_model.py
+++ projects/check_merge_model.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model = AutoModelForCausalLM.from_pretrained("./SinLlama_merged")
-tokenizer = AutoTokenizer.from_pretrained("./SinLlama_merged")
+model = AutoModelForCausalLM.from_pretrained("./SinLlama_merged_bf16")
+tokenizer = AutoTokenizer.from_pretrained("./SinLlama_merged_bf16")
 
 print(model.get_input_embeddings().weight.shape)
 print(model.lm_head.weight.shape)
